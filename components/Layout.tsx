613702010d8fe25331a54eaead1a9dd96036a1fc
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-12-13 16:39:07 +0100

    fonts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,31 @@
-import React, { ReactNode } from 'react'
-import Head from 'next/head'
+import React, { ReactNode } from "react"
+import Head from "next/head"
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
+
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <link
+        rel="preload"
+        href="/fonts/Roboto-Regular.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SpaceMono-Regular.ttf"
+        as="font"
+        crossOrigin=""
+      />
     </Head>
     {children}
   </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import Image from "next/image"
+import Layout from "../components/Layout"
 import { bgWrap } from "../css/index.module.css"
 
 const IndexPage = () => (
