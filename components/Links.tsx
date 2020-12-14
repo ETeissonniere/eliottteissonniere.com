88d9cf2048e843b9213bdc61492c975005cd9df1
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-12-14 11:43:35 +0100

    add hamburger menu for mobile

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
-import { linksMenu } from '../css/links.module.css';
+import { scaleDown as Menu } from 'react-burger-menu';
+import { linksMenu, menuUl, mobileMenuElem } from '../css/links.module.css';
 
 type Props = {
   links: Array<{ name: string; link: string }>;
 };
 
 const Links = ({ links }: Props) => (
-  <div className={linksMenu}>
-    <ul>
-      {links.map((item) => (
-        <li>
-          <Link href={item.link}>
-            <a>{item.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+  <div>
+    <div className={linksMenu}>
+      <ul className={menuUl}>
+        {links.map((item) => (
+          <li>
+            <Link href={item.link}>
+              <a>{item.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <Menu right customCrossIcon={<img src="images/cross.svg" />}>
+      <ul className={menuUl}>
+        {links.map((item) => (
+          <li>
+            <Link href={item.link}>
+              <a className={mobileMenuElem}>{item.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Menu>
   </div>
 );
 
